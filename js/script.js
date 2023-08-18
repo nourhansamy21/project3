893b975812a73e6036620cf6651044b1679a9e8a
Nourhan Samy <137513208+nourhansamy21@users.noreply.github.com>
2023-08-18 13:44:57 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("copy").onclick = function () {
+    alert("text cpoied")
+}
+document.getElementById("copy1").onclick = function () {
+    alert("text cpoied")
+}
+document.getElementById("copy2").onclick = function () {
+    alert("text cpoied")
+}
+
